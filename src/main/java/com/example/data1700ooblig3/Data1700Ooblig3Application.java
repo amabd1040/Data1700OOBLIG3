6956e61c15fd1ebdfbaa620dc78441424abe3411
amabd1040 <amabd1040@oslomet.no>
2024-04-25 19:48:13 +0200

    Initial commit

---
+++ src/main/java/com/example/data1700ooblig3/Data1700Ooblig3Application.java
@@ -0,0 +1,13 @@
+package com.example.data1700ooblig3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Ooblig3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data1700Ooblig3Application.class, args);
+	}
+
+}
